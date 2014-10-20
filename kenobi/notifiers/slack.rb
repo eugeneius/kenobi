6f2c668ac5d6537794ca464e60bf8b04c559a004
Eugene Kenny <elkenny@gmail.com>
2014-10-20 10:55:57 +0100

    These can be set through the integration instead

--- kenobi/notifiers/slack.rb
+++ kenobi/notifiers/slack.rb
@@ -3,8 +3,6 @@ require 'slack-notifier'
 module Kenobi
   module Notifiers
     class Slack
-      ICON_URL = 'http://upload.wikimedia.org/wikipedia/en/3/32/Ben_Kenobi.png'
-
       def self.setup(config)
         if config.key?('SLACK_TEAM') && config.key?('SLACK_TOKEN')
           self.new(config)
@@ -13,16 +11,10 @@ module Kenobi
 
       def initialize(config)
         @notifier = ::Slack::Notifier.new(config['SLACK_TEAM'], config['SLACK_TOKEN'])
-
-        @notifier.username = "Kenobi"
-
-        if config.key?('SLACK_CHANNEL')
-          @notifier.channel = config['SLACK_CHANNEL']
-        end
       end
 
       def notify!(payload)
-        @notifier.ping(message(payload), icon_url: ICON_URL)
+        @notifier.ping(message(payload))
       end
 
       private
